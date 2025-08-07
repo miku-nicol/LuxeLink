8397ff1e5babbdff4cb9d5fae6c4e5f8310ef93d
miku-nicol <miracleuwaifo9@gmail.com>
2025-08-07 01:50:25 +0100

    fix Product import path for vercel build

--- my project/LuxeLink/src/pages/home/Homepage.jsx
+++ my project/LuxeLink/src/pages/home/Homepage.jsx
@@ -7,7 +7,7 @@ import Footer from '../footersec/Footer';
 import Signin from '../../pages/auth/Signin';
 import Signup from '../../pages/auth/Signup';
 import Nav from '../../components/nav/Nav';
-import Cart from '../../components/ui/cart';
+import Cart from '../../components/ui/Cart';
 import CartSidebar from '../../components/ui/cartside';
 
 
